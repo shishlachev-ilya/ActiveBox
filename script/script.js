62a32816ddd5a4b29f1feca09962f1a5c47539dd
Shishlachev-ilya <mailforwot43@mail.ru>
2016-08-28 01:01:24 +0300

    new nav

--- script/script.js
+++ script/script.js
@@ -1,20 +1,64 @@
 $(function(){
-    $(window).scroll(function() {
-    $('.mov').each(function(){
-      var imagePos = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if (imagePos < topOfWindow+800) {
-        $(this).addClass('tada');
-      }
-    });
-  });
+   
+	/* подключение анимации */
+    new WOW().init();
     
-    
-    
+	/* анимация в блоке works */
     $('.link img').hover(function(e){
         $(this).animate({ marginLeft:'-50px', marginTop:'-50px', height:'400px'}, 500);
     }, function(e){
         $(this).animate({ marginLeft:'0', marginTop:'0', height:'350px'}, 500);
     });
-    
+	
+	/* анимированная навигация */
+    $(window).scroll(function() {
+		if ($(this).scrollTop() > 1){
+			$('.navigation').addClass("scroll");
+		}
+		else{
+			$('.navigation').removeClass("scroll");
+		}
+	});
+	
+	/* плавный скрол */
+	$(".navigation").on("click","a", function (event) {
+
+		event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+
+    });
+	
+	
+	/* горизонтальное меню */
+	function menu() {
+
+        var trig = $('.trigger-js'),
+            menu = $('.menu-js');
+
+        trig.click(function() {
+            $(this).next(menu).slideToggle();
+        });
+
+        $(window).resize(function() {
+
+            if ($(window).width() > 732) {
+                menu.removeAttr('style');
+            }
+        });
+    }
+    menu();
+	
+	$('.menu-js li a').click(function(){
+		if ($(window).width() < 992) {
+            $('.menu-js').hide();
+        }else{
+			$('.menu-js').show();
+		}
+	});
+	
 });
+
+
+	
